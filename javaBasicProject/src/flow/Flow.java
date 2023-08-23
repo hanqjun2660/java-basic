0b93ed0c094453731f7e70e824046896062d6032
KANG\b2000 <hanqjun2660@gmail.com>
2023-08-23 12:48:21 +0900

    while문 continue 추가

--- javaBasicProject/src/flow/Flow.java
+++ javaBasicProject/src/flow/Flow.java
@@ -404,4 +404,31 @@ public class Flow {
             System.out.println(i);
         }
     }
+
+    // while문의 continue문을 만나면 while문의 조건식으로 이동한다.
+    public static void FlowEx21() {
+        int menu = 0;
+        int num = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("(1) squere");
+            System.out.println("(2) squere root");
+            System.out.println("(3) log");
+
+            String tmp = sc.nextLine();
+            menu = Integer.parseInt(tmp);
+
+            if(menu == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if(!(1 <= menu && menu <= 3)) {
+                System.out.println("메뉴를 잘못 선택하셨습니다.");
+                continue;
+            }
+
+            System.out.println("선택하신 메뉴는 " + menu + " 번 입니다.");
+        }
+    }
 }
